37c9997e3ab80be2f5305c2abda5dab2673557af
Koderkup <koderkup@gmail.com>
2023-05-16 09:22:58 +0300

    rename file mainpages-pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { DataProvider } from "./GlobalState";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/headers/Header";
-import MainPages from "./components/mainpages/Pages";
+import MainPages from "./components/pages/Pages";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
